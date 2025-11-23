1bbc1a7210a4d093eae2ea2cc07fa5ea28614370
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2025-11-23 16:47:16 +0900

    単体テストを削除

--- tests/TR.Maui.AnchorPopover.UnitTests/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace TR.Maui.AnchorPopover.UnitTests;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
